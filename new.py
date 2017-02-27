875549fff5e4beeb0763149ec2a37ae930d3ca6f
lydbupt123 <lydliu@bupt.edu.cn>
2017-02-27 17:24:48 +0800

    new
    new

--- new.py
+++ new.py
@@ -15,10 +15,15 @@ city=['黄冈', '三亚', '汉中', '宝鸡', '泉州', '石家庄', '东营', '
        '佛山']
 citys={}
 for i,c in enumerate(city):
-    citys[c]=i
+    citys[c]=str(i+1)
 #print(citys)
+food_list=['东北菜', '闽菜', '粤菜', '砂锅/煲类/炖菜', '米粉/米线', '其它烘焙糕点', '中式烧烤', '饮品/甜点', '西北菜', '其它休闲食品', '其它快餐', '中式快餐', '日韩料理', '咖啡', '西式快餐', '其他餐饮美食', '奶茶', '香锅/烤鱼', '川味/重庆火锅', '自助餐', '熟食', '粥', '麻辣烫/串串香', '其它小吃', '冰激凌', '美食特产', '其它地方菜', '江浙菜', '面包', '川菜', '海鲜', '其它烧烤', '湖北菜', '上海本帮菜', '西餐', '其它火锅', '面点', '台湾菜', '湘菜', '咖啡厅', '蛋糕', '生鲜水果', '零食']
+food_dict={}
+for i,f in enumerate(food_list):
+    food_dict[f]=str(i+1)
 wr1=open('超市便利店.csv','a')
 wr2=open('美食.csv','a')
+wr2.write('shop_id,city_id,location_id,per_pay,score,comment_cnt,shop_level,type\n')
 wr3=open('休闲娱乐.csv','a')
 wr4=open('美发美容美甲.csv','a')
 wr5=open('购物.csv','a')
@@ -27,49 +32,52 @@ wr6=open('医疗健康.csv','a')
 with codecs.open('shop_info.txt','r',encoding="utf-8") as f:
     lines = f.readlines()
     for line in lines:
+        line=line.replace('\n','')
         array=line.split(',')
         line_new=''
+        array[1]=citys[array[1]]
         if array[7]=='超市便利店':
             for i in range(10):
                 if i!=9:
                     line_new+=(array[i]+',')
                 else:
-                    line_new+=(array[i])
+                    line_new+=(array[i]+'\n')
             wr1.write(line_new)
         elif array[7]== '美食':
-            for i in range(10):
-                if i!=9:
+            array[-1]=food_dict[array[-1]]
+            for i in range(8):
+                if i !=7:
                     line_new+=(array[i]+',')
                 else:
-                    line_new+=(array[i])
+                    line_new+=(array[-1]+'\n')
             wr2.write(line_new)
         elif array[7]=='休闲娱乐':
             for i in range(10):
                 if i!=9:
                     line_new+=(array[i]+',')
                 else:
-                    line_new+=(array[i])
+                    line_new+=(array[i]+'\n')
             wr3.write(line_new)
         elif array[7]=='美发/美容/美甲':
             for i in range(10):
                 if i!=9:
                     line_new+=(array[i]+',')
                 else:
-                    line_new+=(array[i])
+                    line_new+=(array[i]+'\n')
             wr4.write(line_new)
         elif array[7]=='购物':
             for i in range(10):
                 if i!=9:
                     line_new+=(array[i]+',')
                 else:
-                    line_new+=(array[i])
+                    line_new+=(array[i]+'\n')
             wr5.write(line_new)
         else:
             for i in range(10):
                 if i!=9:
                     line_new+=(array[i]+',')
                 else:
-                    line_new+=(array[i])
+                    line_new+=(array[i]+'\n')
             wr6.write(line_new)
         
     wr1.close()
